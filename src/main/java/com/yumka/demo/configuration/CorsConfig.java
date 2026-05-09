0759afbc5d611e89e3ff5276ba2707ca62999b50
Valera141 <corlando246gmail.com>
2026-05-09 12:48:43 -0600

    configuraciones properties, cors y pom

--- src/main/java/com/yumka/demo/configuration/CorsConfig.java
+++ src/main/java/com/yumka/demo/configuration/CorsConfig.java
@@ -3,16 +3,14 @@ package com.yumka.demo.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.lang.NonNull;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer{
     @Override
-  public void addCorsMappings(@NonNull CorsRegistry registry) {
-    registry.addMapping("/api/v1/**")
-        .allowedOrigins("https://tu-frontend.com") // evitar "*" en producción
-        .allowedMethods("GET","POST","PUT","DELETE","PATCH","OPTIONS")
-        .allowedHeaders("*");
-        //.allowCredentials(true);   // si vas a enviar cookies/autenticación, pero entonces el test retornará el localhost
-}
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+            .allowedOrigins("*") // Permite cualquier origen temporalmente
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowedHeaders("*");
+    }
 }
